0b4bb24a599aa6aa43e0b4c1e45c0848c112171c
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-04-15 13:24:38 -0700

    Added Weakness
    Added weaknesses to test why my first commit did not take the summary and description i added. I'm Pretty sure I didn't select it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {name: 'Charizard', height: 5.58, types: ['Fire', 'Flying']},
-    {name: 'Venusaur', height: 6.58, types: ['Grass', 'Poison']},
-    {name: 'Beedrill', height: 3.25, types: ['Bug', 'Poison']},
+    {name: 'Charizard', height: 5.58, types: ['Fire', 'Flying'], weaknesses: ['Water', 'Electric', 'Rock']},
+    {name: 'Venusaur', height: 6.58, types: ['Grass', 'Poison'], weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic']},
+    {name: 'Beedrill', height: 3.25, types: ['Bug', 'Poison'], weaknesses: ['Fire', 'Flying', 'Psychic', 'Rock']},
 ]
