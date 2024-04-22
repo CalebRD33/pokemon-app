9d7036ad90f8c5d9dcb3b81947cfdec0d138277a
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-04-22 16:00:12 -0700

    Added loop to list pokemon
    -Added a for loop to list out pokemon name and height.
    -added a conditional to identify any pokemon over 6ft tall

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 let pokemonList = [
-    {name: 'Charizard', height: 5.58, types: ['Fire', 'Flying'], weaknesses: ['Water', 'Electric', 'Rock']},
-    {name: 'Venusaur', height: 6.58, types: ['Grass', 'Poison'], weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic']},
-    {name: 'Beedrill', height: 3.25, types: ['Bug', 'Poison'], weaknesses: ['Fire', 'Flying', 'Psychic', 'Rock']},
-]
+    {
+        name: 'Charizard',
+        height: 5.58,
+        types: ['Fire', 'Flying'],
+        weaknesses: ['Water', 'Electric', 'Rock']
+    },
+    {
+        name: 'Venusaur',
+        height: 6.58,
+        types: ['Grass', 'Poison'],
+        weaknesses: ['Fire', 'Ice', 'Flying', 'Psychic']
+    },
+    {
+        name: 'Beedrill',
+        height: 3.25,
+        types:['Bug', 'Poison'],
+        weaknesses: ['Fire', 'Flying', 'Psychic', 'Rock']
+    },
+];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    //loop through each object in the array
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'ft)');
+    //check the height of each pokemon
+    if (pokemonList[i].height > 6) {
+        document.write(' -Wow. That\'s big!')
+    };
+    document.write('<br>')
+}
+
